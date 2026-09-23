4dd89c520e068e88aa5bd29701962984d0940fa6
ValentinPetit11 <valentin.petit@iut-rodez.fr>
2026-09-23 16:39:03 +0200

    Add files via upload

---
+++ etape2.java
@@ -0,0 +1,27 @@
+public class Utils {
+
+    public static int writeInt(byte[] memory, int offset, int value) {
+        memory[offset]     = (byte) (value >>> 24);
+        memory[offset + 1] = (byte) (value >>> 16);
+        memory[offset + 2] = (byte) (value >>> 8);
+        memory[offset + 3] = (byte) (value);
+        return 4;
+    }
+
+    public static int readInt(byte[] memory, int offset) {
+        return ((memory[offset]) << 24)
+             | ((memory[offset + 1]) << 16)
+             | ((memory[offset + 2]) << 8)
+             |  (memory[offset + 3]);
+    }
+
+    public static int writeShort(byte[] memory, int offset, short value) {
+        memory[offset] = (byte) (value >>> 8);
+        memory[offset + 1] = (byte) (value);
+        return 2;
+    }
+
+    public static short readShort(byte[] memory, int offset) {
+        
+    }
+}
